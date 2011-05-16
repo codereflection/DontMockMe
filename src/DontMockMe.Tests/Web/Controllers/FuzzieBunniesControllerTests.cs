8c82988ddec0d1451bddbdc71339e46fc28314e4
codereflection <jeff@codingreflection.com>
2011-05-16 13:55:53 -0700

    Reworking test structure

--- src/DontMockMe.Tests/Web/Controllers/FuzzieBunniesControllerTests.cs
+++ src/DontMockMe.Tests/Web/Controllers/FuzzieBunniesControllerTests.cs
@@ -1,28 +1,38 @@
+using System;
 using System.Web.Mvc;
 using DontMockMe.Web.Controllers;
 using Xunit;
 
 namespace DontMockMe.Tests.Web.Controllers
 {
-    public abstract class with_a_fuzzie_bunny_controller
+    public abstract class SpecFor<T> where T : class 
+    {
+        protected SpecFor()
+        {
+            Context();
+            Because();
+        }
+
+        public abstract void Context();
+        public abstract void Because();
+
+    }
+
+    public abstract class with_a_fuzzie_bunny_controller : SpecFor<FuzzieBunniesController>
     {
         protected FuzzieBunniesController controller;
 
-        protected with_a_fuzzie_bunny_controller()
+        public override void Context()
         {
             controller = new FuzzieBunniesController();
-
-            BecauseOf();
         }
-
-        public abstract void BecauseOf();
     }
 
     public class when_getting_a_list_of_fuzzie_bunnies : with_a_fuzzie_bunny_controller
     {
         private ActionResult result;
 
-        public override void BecauseOf()
+        public override void Because()
         {
             result = controller.Index();
         }
